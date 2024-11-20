b620779d633e672dbcee7ed77fdb5a1e1829a84f
nastiayurash <nasstyayurash@gmail.com>
2024-11-20 17:02:08 +0000

    Deploying to gh-pages from @ nastiayurash/goit-js-hw-09@8a5d3536b4ca21bd33c390f41a7fc0e65e28f41e 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=document.querySelector(".feedback-form");e.elements.email;e.elements.message;
+//# sourceMappingURL=2-form.js.map
